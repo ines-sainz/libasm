97340528e825e97579ecd04166ff48b6c8b0bcec
Inés Sainz <ines.sainz13@gmail.com>
2026-09-15 12:18:41 +0200

    list_push_front.c works, now starting list_push_front.s

--- ft_list_push_front.c
+++ ft_list_push_front.c
@@ -1,14 +1,14 @@
+#include "bonus.h"
+
 t_list	*ft_create_elem(void *data)
 {
 	t_list	*tmp;
 
 	tmp = malloc(sizeof(t_list));
-	if (tmp != NULL)
-	{
-		tmp->data = data;
-		tmp->next = NULL;
-	}
-
+	if (tmp == NULL)
+		return (NULL);
+	tmp->data = data;
+	tmp->next = NULL;
 	return tmp;
 }
 
@@ -16,9 +16,34 @@ void	ft_list_push_front1(t_list **begin_list, void *data)
 {
 	t_list	*node;
 	node = ft_create_elem(data);
-	if (node != NULL)
+	if (node == NULL)
+		return ;
+	if (*begin_list == NULL)
+	{
+		*begin_list = node;
+	}
+	else
 	{
 		node->next = *begin_list;
 		*begin_list = node;
 	}
+}
+
+int main()
+{
+	t_list	*lista;
+	t_list	*tmp;
+
+	lista = NULL;
+
+	ft_list_push_front1(&lista, "elemento1");
+    ft_list_push_front1(&lista, "elemento2");
+
+	while (lista != NULL)
+    {
+        printf("Contenido: %s\n", (char *)lista->data);
+		tmp = lista;
+		lista = lista->next;
+		free(tmp);
+    }
 }
